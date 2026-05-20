d067b9cf4096a74abe501e9a56424a210f12b6f1
FangXin <fangxin246@gmail.com>
2026-05-20 10:40:48 +0800

    整理pinia模块

--- src/stores/modules/User.js
+++ src/stores/modules/User.js
@@ -1,36 +1,35 @@
 import { defineStore } from 'pinia' //创建store用的
 import { ref } from 'vue' //创建响应式数据用的
 import { userGetInfoService } from '@/api/user' //调用后端接口用的
-// 用户模块
+// 用户模块仓库
 export const useUserStore = defineStore(
-  //defineStore（）创建一个 store
   'big-user',
-  //这个big-user就是这个store的id，相当于仓库编号,pinia会用它来区分不同的store
   () => {
+    //这个big-user就是这个store的id，相当于仓库编号,pinia会用它来区分不同的store
+
     const token = ref('')
+    const user = ref({})
+    //实现了token的CRUD操作
     const setToken = (newToken) => {
       token.value = newToken
     }
-    const removeToken = () => {
-      token.value = ''
-    }
-    const user = ref({})
-
     const getUser = async () => {
       const res = await userGetInfoService()
       user.value = res.data
     }
-
     const setUser = (obj) => {
       user.value = obj
     }
+    const removeToken = () => {
+      token.value = ''
+    }
     return {
       token,
-      setToken,
-      removeToken,
       user,
       getUser,
+      setToken,
       setUser,
+      removeToken,
     }
   },
   {
